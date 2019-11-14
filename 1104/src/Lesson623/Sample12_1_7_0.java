a6bfbe4d69838f9aea2fc85819b293246a6908fd
Surf07 <k-iida@tbz.t-com.ne.jp>
2019-11-14 22:29:51 +0900

    20191114のコミット

---
+++ 1104/src/Lesson623/Sample12_1_7_0.java
@@ -0,0 +1,28 @@
+package Lesson623;
+
+
+
+class Super{
+	int num;
+}
+
+class Sub extends Super{
+	int num;
+
+	public void get(int n) {
+		num = n;
+		super.num = n*100;
+		super.num = n*10000;
+	}
+}
+
+public class Sample12_1_7_0 {
+	public static void main(String[] args) {
+		Sub sb = new Sub();
+		sb.get(100);
+		System.out.println(sb.num);
+
+		Super spr = sb;
+		System.out.println(spr.num);
+	}
+}
